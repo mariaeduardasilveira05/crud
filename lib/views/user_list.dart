06feed55ed221f6d13417ffb373bdfa23f65daaa
Lorenzo Rossoni <lorenzorossoni080403@gmail.com>
2024-09-17 20:28:25 -0300

    commit inicial.

---
+++ lib/views/user_list.dart
@@ -0,0 +1,24 @@
+import 'package:crud/components/user_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:crud/provider/users.dart';
+
+class UserList extends StatelessWidget {
+  const UserList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Users users = Provider.of(context);
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Lista de Usuários'),
+          actions: <Widget>[
+            IconButton(onPressed: () {}, icon: const Icon(Icons.add))
+          ],
+        ),
+        body: ListView.builder(
+            itemCount: users.count,
+            itemBuilder: (ctx, i) => UserTile(users.byIndex(i))));
+  }
+}
